64786b3af313eb40edb36a388782ca5c57b740bf
jaredpa29 <dallon.glick@gmail.com>
2016-10-29 17:06:51 -0600

    first

--- Lab_5/VGA.tcl
+++
@@ -1,9 +0,0 @@
-restart
-
-isim force add clk 1 -value 0 -time 20 ns -repeat 40 ns
-
-put rst 1
-run 2us
-
-put rst 0
-run 17ms
--- Lab_8/rx.tcl
+++
@@ -1,3 +0,0 @@
-restart
-
-isim force add clk 1 -value 0 -time 10 ns -repeat 40 ns
